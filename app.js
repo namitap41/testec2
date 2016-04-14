f38fdca31c86e47bd9725eb776815c6e116c25b3
namitap41 <namita@particle41.com>
2016-04-14 11:29:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ http.createServer(function (request, response) {
  
    response.writeHead(200, {'Content-Type': 'text/plain'});
     
-   response.end('amittttttttttttttt');
+   response.end('amitttttttttttttttt');
    
 }).listen(3000);
